7dcade6dee7723d69d5e7ff73434fe4ccc08e199
qloog <quanlongwang@gmail.com>
2024-04-15 14:06:54 +0800

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs'
+import { Analytics } from '@vercel/analytics/react';
+
 import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
@@ -27,6 +29,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
